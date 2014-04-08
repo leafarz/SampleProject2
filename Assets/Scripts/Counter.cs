7f8fa82f9fa380536b7de363d49e7e6ce4fa279f
Raf Formes <rafformes@S6-MM-TUAZON.local>
2014-04-08 17:29:13 +0800

    sadfwe

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Counter{
+	public static int counter =0;
+	public static int timer=0;
+}
---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClick : MonoBehaviour {
+	public int type;
+	private Script1 s;
+	private Transform t;
+
+	void Start(){
+		t = transform;
+		s = t.parent.GetComponent<Script1> ();
+		type = Random.Range (1, 4);
+		switch (type) {
+			case 1:{
+				renderer.material.color=Color.red;
+				break;
+			}case 2:{
+				renderer.material.color=Color.green;
+				break;
+			}case 3:{
+				renderer.material.color=Color.yellow;
+				break;
+			}
+		}
+	}
+
+	public void Check(){
+		s.CheckSides (t.position, type);
+	}
+
+	void OnMouseDown(){
+		Check();
+	}
+}
